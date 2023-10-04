33167b2c25cfd34af49a991ccd7c0c10bfb4f1fd
radik097 <radikmoroz98@gmail.com>
2023-10-04 03:19:42 +0200

    deleted:    automation.py 	deleted:    detect.py 	deleted:    gui.py 	modified:   pyproject.toml

--- automation.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-
-def click_button(location):
-    pyautogui.click(location)
-
-# Usage
-location = (100, 200)  # Replace with actual location
-click_button(location)
--- gui.py
+++
@@ -1,9 +0,0 @@
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.overrideredirect(True)  # Remove window decorations
-    root.wm_attributes('-topmost', True)  # Keep window always on top
-    coords_mouse = tk.Toplevel(root)
-    app = App(root, coords_mouse)
-    root.mainloop()
